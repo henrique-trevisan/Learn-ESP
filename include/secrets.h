d16f37daf39499800606b74e6a8fb9c7753e4938
Henrique Trevisan <trehen30@gmail.com>
2025-12-28 12:03:49 -0300

    Update Wi-Fi credentials in secrets.h and adjust upload/monitor ports in platformio.ini

--- include/secrets.h
+++ include/secrets.h
@@ -2,5 +2,7 @@
 #pragma once
 
 // Wi-fi SSID and password
-static constexpr const char* WIFI_SSID = "Claro_202_2G";
-static constexpr const char* WIFI_PASS = "Helen@nodar1";
+//static constexpr const char* WIFI_SSID = "Claro_202_2G";
+//static constexpr const char* WIFI_PASS = "Helen@nodar1";
+static constexpr const char* WIFI_SSID = "Getel Telecom - Alterio";
+static constexpr const char* WIFI_PASS = "trevisan";
